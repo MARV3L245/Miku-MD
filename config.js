f87e1843fe08eba139ce8002372e648c0f409c8c
MARV3L245 <122432051+MARV3L245@users.noreply.github.com>
2023-01-11 09:29:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['08098566023','08057044049']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['08098566023','08057044049']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['08098566023','08057044049']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.Owner = ['2348098566023','2348057044049']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['2348098566023','2348057044049']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['2348098566023','2348057044049']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.BotName = "Miku"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.OwnerName = "Marv3l"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
